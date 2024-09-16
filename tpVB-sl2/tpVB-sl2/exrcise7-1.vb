961da6c551235dddb959e5264bbb03cdcee72487
aelig.beauvir.rabelais <lemmy12347@gmail.com>
2024-09-16 17:04:41 +0200

    Ex7 fini, commence ex 9

---
+++ tpVB-sl2/tpVB-sl2/exrcise7-1.vb
@@ -0,0 +1,30 @@
+﻿Module exercise_7_1
+    Sub main()
+
+        Dim montant, remise, montantNet As Double
+
+        Console.WriteLine("veuiller taper votre montant")
+        montant = Console.ReadLine()
+        remise = 0
+
+        If montant > 5000 Then
+            remise = 2 / 100
+            Console.WriteLine("remise de 2%")
+
+        ElseIf montant >= 2000 Then ' montant compris entre 2000 et 5000
+            remise = 1 / 100
+            Console.WriteLine("remise de 1%")
+        Else
+            Console.WriteLine("pas de remise")
+
+        End If
+
+        montantNet = montant * (1 - remise)
+
+        Console.WriteLine("Le montant net est : " + montantNet.ToString())
+        Console.WriteLine("Au revoir")
+        Console.ReadLine()
+    End Sub
+End Module
+
+
